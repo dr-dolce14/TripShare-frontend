fd88a7dc8a6f07c79b6c0ce1729efee7d613686c
dr-dolce14 <dolcemascop1@gmail.com>
2021-03-27 11:59:40 -0400

    was able to iterate through the array of participants for each trip and show them under the appropriate trip

--- src/TripItem.js
+++ src/TripItem.js
@@ -1,12 +1,24 @@
 import React from 'react' 
 
 class TripItem extends React.Component {
+
+    renderUsers = () => {
+        return this.props.trip.users.map((userObj) => 
+        <li key={userObj.id}>
+            Name: {userObj.name}
+        </li>)
+    }
+
     render() {
         return(
             <div className={this.props.key}>
-                <h1>{this.props.trip.name} Trip!</h1>
-                <p>{this.props.trip.location}</p>
-                <p>Led by: {this.props.trip.leader}</p>
+                <h3>Trip Name: {this.props.trip.name}</h3>
+                <p>Trip Location: {this.props.trip.location}</p>
+                <p>Led by: {this.props.trip.user_id}</p>
+                <p>Participants:</p>
+                <ol>
+                    {this.renderUsers()}
+                </ol>
             </div>
         )
     }
